ce2492c3e6303b0db22dc172b6bd3a9e60cde7ca
andyfiveeleven <fouracesguitar@gmail.com>
2017-06-14 16:35:18 -0700

    author and category filtering enabled

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,11 +40,12 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('article').hide();
+      $('article[data-author="this.val()"]').show();
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').show();
     }
     $('#category-filter').val('');
   });
@@ -55,6 +56,15 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="this.val()"]').show();
+    } else {
+      $('article').show();
+    }
+    $('#author-filter').val('');
+  });
 
 };
 
