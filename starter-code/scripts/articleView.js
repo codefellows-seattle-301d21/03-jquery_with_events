c56d4a9bfec40743acbbe7358a8fcb34a9dd4611
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-14 16:34:12 -0700

    Added filter functionality.

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,11 +40,13 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('article').hide();
+      $('article[data-author="' + $(this).val() +'"]').fadeIn();
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').show();
+      $('.template').hide();
     }
     $('#category-filter').val('');
   });
@@ -55,7 +57,16 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() +'"]').fadeIn();
+    } else {
+      $('article').show();
+      $('.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
