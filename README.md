52d39bc91779dad5ab52d5ea6ccc047585cf8c0f
bnates <brian.nations@codefellows.com>
2017-06-14 15:54:40 -0700

    added lab req

---
+++ README.md
@@ -0,0 +1,54 @@
+![CF](https://i.imgur.com/7v5ASc8.png)  Lab 03: jQuery and events
+=======
+[Code of Conduct](https://github.com/codefellows/code-of-conduct)
+
+## Submission Instructions
+When you are finished with lab, follow these steps to submit your work. Create one Pull Request (aka: "PR") from your Forked repo to the CF repo with your changes, and you'll each submit that same PR link in Canvas.
+
+1. Ensure that all your local changes are committed, and pushed to your origin repo.
+1. Visit the origin repo on github.com, and ensure that all of your completed work has been merged to master via Pull Requests within your repo.
+1. Create a new PR from your Fork to the CF repo and ensure the branches look correct.
+1. Fill in the template based on the text box prompts:
+  1. Write a good descriptive summary of your changes:
+    1. Be sure to include how much time you spent on it, and who you worked with.
+    1. Briefly reflect on and summarize your process.
+1. When you create the PR, it will have a unique URL. Copy this link, share with your partner, and paste it into the assignment submission form in Canvas. Both the driver and the navigator will submit the same PR link.
+---
+
+## Learning Objectives
+* Execute event bindings with jQuery’s `.on()` method.
+* Comprehend when event delegation is appropriate.
+* Be able to configure event bindings using delegation.
+* Familiarity with `data-<attributes>` & string concatenation.
+* Build dynamic jQuery selector strings.
+* Use of `$(document).ready()`
+* Integration of icon fonts through the use of IcoMoon.
+
+---
+
+## Resources  
+[jQuery API Reference](https://oscarotero.com/jquery/)
+
+---
+
+## Feature Tasks  
+1. Add any new script tags to your HTML.
+2. Complete the new requirements for setting `data-<attributes>` in your `toHtml()` method.
+3. Review and understand the new JS file, `articleView.js`
+4. Complete the methods for handling filter events when selecting an option from a drop down menu via Authors and Categories.
+5. Complete the method for handling tab-based event navigation on the page.
+6. Add an event to reveal a complete article if the user would like to see more than just a two-line teaser.
+
+#### Stretch Goal
+
+- After implementing the "Read On ->" link that shows the remainder of an article beyond the teaser, change the text of the link to "Show Less <-" and hide everything but the teaser.
+
+---
+
+## Rubric  
+Criteria | Pts
+---|---
+Meets all Assignment Reqs | 6
+Uses idiomatic code style | 3
+Follows proper Git workflow | 1
+**Total** | **10**
---
+++ starter-code/scripts/articleView.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
+var articleView = {};
+
+articleView.populateFilters = function() {
+  $('article').each(function() {
+    var authorName, category, optionTag;
+    if (!$(this).hasClass('template')) {
+      // REVIEW: We need to take every author name from the page, and make it an option in the Author filter.
+      //       To do so, Build an `option` DOM element that we can append to the author select box.
+      //       Start by grabbing the author's name from an attribute in `this` article element,
+      //       and then use that bit of text to create the option tag (in a variable named `optionTag`),
+      //       that we can append to the #author-filter select element.
+      authorName = $(this).attr('data-author');
+      optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+
+      if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
+        $('#author-filter').append(optionTag);
+      }
+
+      // REVIEW: Similar to the above, but...
+      //       Avoid duplicates! We don't want to append the category name if the select
+      //       already has this category as an option!
+      category = $(this).attr('data-category');
+      optionTag = '<option value="' + category + '">' + category + '</option>';
+      if ($('#category-filter option[value="' + category + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleAuthorFilter = function() {
+  $('#author-filter').on('change', function() {
+    // REVIEW: Inside this function, "this" is the element that triggered the event handler function we're
+    //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
+    //         onto it.
+    if ($(this).val()) {
+      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      //       and then show just the ones that match for the author that was selected.
+      //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+
+    } else {
+      // TODO: If the select box was changed to an option that is blank, we should
+      //       show all the articles, except the one article we are using as a template.
+
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = function() {
+  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  //       When an option with a value is selected, hide all the articles, then reveal the matches.
+  //       When the blank (default) option is selected, show all the articles, except for the template.
+  //       Be sure to reset the #author-filter while you are at it!
+
+};
+
+articleView.handleMainNav = function() {
+  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
+  //       single .tab-content section that is associated with the clicked .tab element.
+  //       So: You need to dynamically build a selector string with the correct ID, based on the
+  //       data available to you on the .tab element that was clicked.
+
+
+  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+};
+
+articleView.setTeasers = function() {
+  $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any article body.
+
+  // TODO: Add an event handler to reveal all the hidden elements,
+  //       when the .read-on link is clicked. You can go ahead and hide the
+  //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
+  //       process any .read-on clicks that happen within child nodes.
+
+  // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
+
+};
+
+// TODO: Call all of the above functions, once we are sure the DOM is ready.
+$(document).ready(function() {
+
+})
