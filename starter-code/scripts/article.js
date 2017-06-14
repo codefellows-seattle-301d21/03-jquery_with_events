5b712452079d0e55d2af940686b7697bdeac54fe
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-14 16:09:12 -0700

    Added author data attribute to cloned article.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -20,7 +20,7 @@ Article.prototype.toHtml = function() {
   $newArticle.attr('data-category', this.category);
   // TODO: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
   //       Doing so will allow us to use selectors to target articles, based on who wrote them.
-
+  $newArticle.attr('data-author', this.author);
   $newArticle.find('.byline a').html(this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
   $newArticle.find('h1:first').html(this.title);
