bc5014e0d6934fd48643661be2a97c190447ed67
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-06-14 17:18:58 -0700

    Added handler to nav bar items
    Allows users to remove excess items from page.
    Allows Devs to similate multiple pages without loading another HTML file

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -56,10 +56,7 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-  //       When an option with a value is selected, hide all the articles, then reveal the matches.
-  //       When the blank (default) option is selected, show all the articles, except for the template.
-  //       Be sure to reset the #author-filter while you are at it!
+  // Done!!
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
       $('article').filter((ind, art) => $(art).attr('data-category') != $(this).val()).each(
@@ -75,14 +72,19 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
-  //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
-  //       single .tab-content section that is associated with the clicked .tab element.
-  //       So: You need to dynamically build a selector string with the correct ID, based on the
-  //       data available to you on the .tab element that was clicked.
+  // Done
 
 
-  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+  $('.main-nav .tab:first').click(function() {
+    $('#articles').show();
+    $('#about').hide();
+  });
+
+  $('.main-nav .tab:last').click(function() {
+    $('#articles').hide();
+    $('#about').show();
+  });
+   // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 articleView.setTeasers = function() {
