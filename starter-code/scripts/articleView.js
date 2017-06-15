bb2047449955aee43952304a47d5928de1626b9b
Tim Turner <timmyturner@me.com>
2017-06-14 17:22:39 -0700

    saving TODO items for the selector menu

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,11 +40,13 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('article').hide();
+      $('article[data-author="' + $(this).val() + '"]').fadeIn();
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').fadeIn();
+      $('.template').hide();
     }
     $('#category-filter').val('');
   });
@@ -84,5 +86,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
