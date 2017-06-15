7f5416a3c4c2354efbce93fbd42a6c49bc8e445f
Allan Liebold <allan.liebold@gmail.com>
2017-06-14 17:10:18 -0700

    adds event handler for main-nav tabs to show selected tab content

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -74,7 +74,11 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-
+  $('.tab').on('click', function() {
+    $('.tab-content').hide();
+    var $tab = $(this).data('content');
+    $('.tab-content[data-content=$tab]').show();
+  });
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
